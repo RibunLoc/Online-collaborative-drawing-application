8a49b4d52390d0451b79207b54fe8f8fb69a5cb9
Baonguyen1711 <Baonguyen17112004@gmail.com>
2024-12-02 22:18:41 +0700

    add send draw to server

--- ProjectTeam/ProjectTeam/Draw.Designer.cs
+++ ProjectTeam/ProjectTeam/Draw.Designer.cs
@@ -57,7 +57,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1412, 70);
+            this.panel1.Size = new System.Drawing.Size(1009, 50);
             this.panel1.TabIndex = 0;
             // 
             // panelCodeRoom
@@ -65,10 +65,11 @@
             this.panelCodeRoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panelCodeRoom.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.panelCodeRoom.Controls.Add(this.lblCodeRoom);
-            this.panelCodeRoom.Location = new System.Drawing.Point(1073, 12);
+            this.panelCodeRoom.Location = new System.Drawing.Point(766, 9);
+            this.panelCodeRoom.Margin = new System.Windows.Forms.Padding(2);
             this.panelCodeRoom.Name = "panelCodeRoom";
-            this.panelCodeRoom.Padding = new System.Windows.Forms.Padding(0, 0, 0, 4);
-            this.panelCodeRoom.Size = new System.Drawing.Size(327, 43);
+            this.panelCodeRoom.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.panelCodeRoom.Size = new System.Drawing.Size(234, 31);
             this.panelCodeRoom.TabIndex = 4;
             // 
             // lblCodeRoom
@@ -79,10 +80,10 @@
             this.lblCodeRoom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblCodeRoom.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.lblCodeRoom.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblCodeRoom.Location = new System.Drawing.Point(0, 9);
-            this.lblCodeRoom.Margin = new System.Windows.Forms.Padding(3, 0, 3, 4);
+            this.lblCodeRoom.Location = new System.Drawing.Point(0, 8);
+            this.lblCodeRoom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 3);
             this.lblCodeRoom.Name = "lblCodeRoom";
-            this.lblCodeRoom.Size = new System.Drawing.Size(174, 30);
+            this.lblCodeRoom.Size = new System.Drawing.Size(124, 20);
             this.lblCodeRoom.TabIndex = 3;
             this.lblCodeRoom.Text = "mã phòng: 2617";
             this.lblCodeRoom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -98,9 +99,10 @@
             this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton2.IconSize = 40;
             this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.iconButton2.Location = new System.Drawing.Point(140, 0);
+            this.iconButton2.Location = new System.Drawing.Point(100, 0);
+            this.iconButton2.Margin = new System.Windows.Forms.Padding(2);
             this.iconButton2.Name = "iconButton2";
-            this.iconButton2.Size = new System.Drawing.Size(70, 70);
+            this.iconButton2.Size = new System.Drawing.Size(50, 50);
             this.iconButton2.TabIndex = 2;
             this.iconButton2.Text = "Xóa";
             this.iconButton2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -117,13 +119,15 @@
             this.Bar_btn_2.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.Bar_btn_2.IconSize = 40;
             this.Bar_btn_2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Bar_btn_2.Location = new System.Drawing.Point(70, 0);
+            this.Bar_btn_2.Location = new System.Drawing.Point(50, 0);
+            this.Bar_btn_2.Margin = new System.Windows.Forms.Padding(2);
             this.Bar_btn_2.Name = "Bar_btn_2";
-            this.Bar_btn_2.Size = new System.Drawing.Size(70, 70);
+            this.Bar_btn_2.Size = new System.Drawing.Size(50, 50);
             this.Bar_btn_2.TabIndex = 1;
             this.Bar_btn_2.Text = "Vẽ";
             this.Bar_btn_2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.Bar_btn_2.UseVisualStyleBackColor = true;
+            this.Bar_btn_2.Click += new System.EventHandler(this.Bar_btn_2_Click);
             // 
             // Bar_btn1
             // 
@@ -139,7 +143,7 @@
             this.Bar_btn1.Location = new System.Drawing.Point(0, 0);
             this.Bar_btn1.Margin = new System.Windows.Forms.Padding(0);
             this.Bar_btn1.Name = "Bar_btn1";
-            this.Bar_btn1.Size = new System.Drawing.Size(70, 70);
+            this.Bar_btn1.Size = new System.Drawing.Size(50, 50);
             this.Bar_btn1.TabIndex = 0;
             this.Bar_btn1.Text = "Thoát";
             this.Bar_btn1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -148,9 +152,10 @@
             // panel_Draw
             // 
             this.panel_Draw.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel_Draw.Location = new System.Drawing.Point(0, 70);
+            this.panel_Draw.Location = new System.Drawing.Point(0, 50);
+            this.panel_Draw.Margin = new System.Windows.Forms.Padding(2);
             this.panel_Draw.Name = "panel_Draw";
-            this.panel_Draw.Size = new System.Drawing.Size(1412, 770);
+            this.panel_Draw.Size = new System.Drawing.Size(1009, 550);
             this.panel_Draw.TabIndex = 1;
             this.panel_Draw.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Draw_Paint);
             // 
@@ -169,13 +174,13 @@
             this.ádsadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ádasdToolStripMenuItem});
             this.ádsadToolStripMenuItem.Name = "ádsadToolStripMenuItem";
-            this.ádsadToolStripMenuItem.Size = new System.Drawing.Size(141, 36);
+            this.ádsadToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
             this.ádsadToolStripMenuItem.Text = "ádsad";
             // 
             // ádasdToolStripMenuItem
             // 
             this.ádasdToolStripMenuItem.Name = "ádasdToolStripMenuItem";
-            this.ádasdToolStripMenuItem.Size = new System.Drawing.Size(186, 40);
+            this.ádasdToolStripMenuItem.Size = new System.Drawing.Size(132, 26);
             this.ádasdToolStripMenuItem.Text = "ádasd";
             // 
             // rjDropdownMenu2
@@ -188,16 +193,17 @@
             this.rjDropdownMenu2.MenuItemTextColor = System.Drawing.Color.Empty;
             this.rjDropdownMenu2.Name = "rjDropdownMenu2";
             this.rjDropdownMenu2.PrimaryColor = System.Drawing.Color.Empty;
-            this.rjDropdownMenu2.Size = new System.Drawing.Size(142, 40);
+            this.rjDropdownMenu2.Size = new System.Drawing.Size(119, 28);
             // 
             // Draw
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(168F, 168F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1412, 840);
+            this.ClientSize = new System.Drawing.Size(1009, 600);
             this.Controls.Add(this.panel_Draw);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Draw";
             this.Text = "Draw";
             this.Load += new System.EventHandler(this.Draw_Load);
